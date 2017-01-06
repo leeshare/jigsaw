a882f0c9335d9ec2c06ebde9e87d3fdbfc40f264
hp-win10-worker <nkiang@gmail.com>
2017-01-06 16:33:30 +0800

    sth

--- jigsaw_server/src/com/jigsaw/config/AppConfig.java
+++ jigsaw_server/src/com/jigsaw/config/AppConfig.java
@@ -9,10 +9,10 @@ import com.jfinal.config.Routes;
 import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.c3p0.C3p0Plugin;
-import com.jfinal.plugin.ehcache.EhCachePlugin;
 import com.jfinal.plugin.redis.RedisPlugin;
 import com.jfinal.render.ViewType;
 import com.jigsaw.controller.LoginController;
+import com.jigsaw.controller.UserController;
 import com.jigsaw.interceptor.GlobalActionInterceptor;
 import com.jigsaw.interceptor.GlobalServiceInterceptor;
 import com.jigsaw.model._MappingKit;
@@ -28,7 +28,8 @@ public class AppConfig extends JFinalConfig {
 
 	@Override
 	public void configRoute(Routes me) {
-		me.add("/login", LoginController.class);
+		me.add("/user", UserController.class);
+		//me.add("/login", LoginController.class);
 		
 		//me.add(new FrontRoutes());
 		//me.add(new AdminRoutes());
@@ -48,7 +49,7 @@ public class AppConfig extends JFinalConfig {
 		
 		//activeRecordPlugin.setDialect(new PostgresqlDialect());
 		
-		me.add(new EhCachePlugin());
+		//me.add(new EhCachePlugin());
 		
 		String redisHost = PropKit.use("redis_config.txt").get("host");
 		//非第一次加载的配置，则需要每次通过use来制定配置文件名再取值
--- jigsaw_server/src/com/jigsaw/controller/UserController.java
+++ jigsaw_server/src/com/jigsaw/controller/UserController.java
@@ -1,8 +1,24 @@
 package com.jigsaw.controller;
 
+import com.jfinal.core.ActionKey;
 import com.jfinal.core.Controller;
 
 public class UserController extends Controller {
 
-	//public void 
+	@ActionKey("/login")
+	public void index(){
+		
+		
+		 renderJson("{\"message\":\"Please input password!\"}");
+	}
+	
+	@ActionKey("/auth")
+	public void auth(){
+		
+	}
+	
+	@ActionKey("logout")
+	public void logout(){
+		
+	}
 }
