8fe09a553f8d708b71c964df90978c27bd5a1ecf
hp-win10-worker <nkiang@gmail.com>
2017-02-10 17:37:25 +0800

    sth

---
+++ android/app/src/main/java/com/tohappy/lxl/jigsaw/UserActivity.java
@@ -0,0 +1,17 @@
+package com.tohappy.lxl.jigsaw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by worker on 2017/2/10.
+ */
+
+public class UserActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
